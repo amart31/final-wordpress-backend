61cd8538a6b146035f87111db0310d0481acd423
Andy Martinez <martinez1751@gmail.com>
2019-04-25 00:11:04 +0000

    first commit

---
+++ wp-content/themes/rigo/src/php/Controller/SampleController.php
@@ -0,0 +1,51 @@
+<?php
+namespace Rigo\Controller;
+
+use Rigo\Types\Product;
+
+class SampleController{
+    
+    public function getHomeData(){
+        return [
+            'name' => 'Rigoberto'
+        ];
+    }
+    public function getAllProducts(){
+        $query = Product::all();
+        
+        if($query->have_posts()){
+            while($query->have_posts()){
+                $query->the_post();        
+        //Include the Meta Tags and Values
+        $query->post->meta_keys = get_post_meta($query->post->ID);
+        $query->post->image = get_field("image_01",$query->post->ID);
+      }
+    }    
+        return $query->posts;
+    }
+    
+    //get products by user
+    
+    //post function for users to add products
+        public function createProduct($data){
+        $post_arr = array(
+        
+        "post_title" => $data["post_title"],
+        "post_content" => $data["post_content"],
+        "post_type" => "product",
+        "post_author" => get_current_user_id(),
+        "meta_input" => array(
+            "product_name" => $data["product_name"],
+            ),
+        
+        );
+        wp_inser_post($post_arr, $wp_error=true);
+            return ["post added successfully"];
+    }
+    
+    
+    
+}
+?>
+
+       
---
+++ wp-content/themes/rigo/src/php/Types/Product.php
@@ -0,0 +1,10 @@
+<?php
+namespace Rigo\Types;
+    
+use WPAS\Types\BasePostType;
+
+class Product extends BasePostType{
+    
+}
+
+?>
